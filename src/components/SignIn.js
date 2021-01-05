c09e8b29efa455883baa183da20f534774e6dfcf
MorisakiYuji <youkourow@yahoo.co.jp>
2021-01-05 10:57:42 +0900

    useEffectの実装

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -41,6 +41,14 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn({setName}) {
   const classes = useStyles();
+  const [disabled, setDisabled] = useState(true);
+  const [string, setString] = useState('');
+  console.log(string);
+
+  useEffect(() => {
+    const disable = string === '';
+    setDisabled(disable);
+  }, [string]);
 
   return (
     <Container component="main" maxWidth="xs">
@@ -59,6 +67,7 @@ export default function SignIn({setName}) {
             label="ニックネーム"
             name="name"
             autoFocus
+            onChange={(e) => setString(e.target.value)}
           />
           <Button
             type="submit"
@@ -66,6 +75,7 @@ export default function SignIn({setName}) {
             variant="contained"
             color="primary"
             className={classes.submit}
+            disabled={disabled}
           >
             はじめる
           </Button>
