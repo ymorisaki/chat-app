deb9040710123aee6fcb91c357dede1611f6b5a7
MorisakiYuji <youkourow@yahoo.co.jp>
2021-01-05 10:39:38 +0900

    useStateの導入

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -39,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn() {
+export default function SignIn({setName}) {
   const classes = useStyles();
 
   return (
