7edec9ab3df3b4d8a153c02538b2e950a2495eb5
MorisakiYuji <youkourow@yahoo.co.jp>
2021-01-05 15:44:40 +0900

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import SignIn from './SignIn'
 import Main from './Main'
+import config from '../config.json'
 
 export default () => {
   const [name, setName] = useState('')
   console.log(name)
 
-  if (name === '') {
+  if (name === '' && config.signInEnaled) {
     return <SignIn setName={setName} />
   } else {
     return <Main name={name} />
